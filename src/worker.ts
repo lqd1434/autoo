2a602569f6a383f4f3a04f9ca72b684e03e971e6
lqd1434 <1434288209@qq.com>
2021-12-01 00:20:26 +0800

    :art: 美化输出

--- src/worker.ts
+++ src/worker.ts
@@ -1,4 +1,7 @@
-const path = require('path');
+const cliSpinners = require('cli-spinners');
+const cliProgress = require('cli-progress');
+console.log(cliSpinners.star);
+const frames = cliSpinners.dots2.frames;
 const { fork } = require('child_process');
 const { existsSync, readdirSync } = require('fs-extra');
 const { resolveTemplatePath, resolveWriteScriptPath, resolvePath, resolveRootPath } = require('./path');
@@ -13,6 +16,11 @@ export const startWorker = async (change: string, config) => {
 
   //模板文件数组
   const sourceFiles = readdirSync(templatePath);
+
+  const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+  bar1.start(100, 0);
+
+  let pre = 0;
   sourceFiles.forEach((source, index) => {
     //目的地文件
     const targetPath = resolvePath(changePath, `./${source}`);
@@ -23,7 +31,14 @@ export const startWorker = async (change: string, config) => {
     if (!exist) {
       console.log('新增目录==>', change);
       const worker = fork(writeScript, [index]);
+
       worker.send({ targetPath: targetPath, source: sourceFile });
+      worker.on('message', (msg) => {
+        bar1.update(msg);
+        if (msg === 100) {
+          bar1.stop();
+        }
+      });
     }
   });
 };
--- src/write.ts
+++ src/write.ts
@@ -7,8 +7,11 @@ process.on('message', (msg: any) => {
   try {
     writeFileSync(targetPath, content);
     console.log(`创建${targetPath}成功`);
+    process.env.Progress = String(123);
+    process.send(50 * parseInt(process.argv[2]));
   } catch (e) {
     console.log(`创建${targetPath}失败`);
     console.log(e);
+    process.send(1);
   }
 });
